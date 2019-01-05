5b8e5008c51efce42de5f88950d03fd6859bc214
frox <mreps@inboxen.org>
2019-01-05 20:01:47 +0100

    fix map page with fullscreen and minimap

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -45,9 +45,26 @@ class Home extends Component {
         ) : (
           <Indicators mapData={mapData} />
         )}
-
-        <Divider variant="middle" />
         <iframe
+          className={classes.iframe}
+          width="100%"
+          height={window.innerHeight * 0.6}
+          frameBorder="0"
+          allowfullscreen="true"
+          title="uMap ifram"
+          src="https://umap.openstreetmap.fr/fr/map/en-selle-pour-pekin_266919?scaleControl=true&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=false&embedControl=false&datalayersControl=true&onLoadPanel=undefined&captionBar=false&fullscreenControl=true&locateControl=false&measureControl=false&editinosmControl=false"
+        >
+          <p>Your browser does not support iframes.</p>
+        </iframe>
+        <Divider variant="middle" />
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(Home);
+
+/*        <iframe
           className={classes.iframe}
           width="100%"
           height={window.innerHeight * 0.6}
@@ -57,10 +74,4 @@ class Home extends Component {
           title="uMap ifram"
         >
           <p>Your browser does not support iframes.</p>
-        </iframe>
-      </React.Fragment>
-    );
-  }
-}
-
-export default withStyles(styles, { withTheme: true })(Home);
+        </iframe>*/
--- src/views/OSMapPage/OSMapPage.jsx
+++ src/views/OSMapPage/OSMapPage.jsx
@@ -18,9 +18,9 @@ const OSMapPage = ({ classes }) => {
       width="100%"
       height={window.innerHeight * 0.8}
       frameBorder="0"
-      allowfullscreen
-      src="https://umap.openstreetmap.fr/fr/map/en-selle-pour-pekin_266919?scaleControl=true&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=false&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&fullscreenControl=true&measureControl=false&editinosmControl=false"
+      allowfullscreen="true"
       title="uMap ifram"
+      src="https://umap.openstreetmap.fr/fr/map/en-selle-pour-pekin_266919?scaleControl=true&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=false&embedControl=false&datalayersControl=true&onLoadPanel=undefined&captionBar=false&fullscreenControl=true&locateControl=false&measureControl=false&editinosmControl=false"
     >
       <p>Your browser does not support iframes.</p>
     </iframe>
